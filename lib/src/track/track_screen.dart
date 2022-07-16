02422d37a0d19bfd12d25612aa7a924d97fe0551
Sam Moore <sam@betterment.com>
2022-07-16 11:20:06 -0400

    setup go_router

---
+++ lib/src/track/track_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TrackScreen extends StatelessWidget {
+  const TrackScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Track'),
+        ),
+      ),
+    );
+  }
+}
